3a13889f1a04ac028fe1024c507a6aae3d7a13d1
kalanaS95 <kalana.sahabandu@gmail.com>
2020-05-12 19:20:41 -0700

    fixed login path

--- models/SubUnits.js
+++ models/SubUnits.js
@@ -1069,7 +1069,7 @@ module.exports.loginUser = async function(UWID,callback){
         var jsonObject = temp.map(JSON.stringify); 
         var uniqueSet = new Set(jsonObject); 
         var uniqueArray = Array.from(uniqueSet).map(JSON.parse); 
-        possible_roles.approver.push(uniqueArray);
+        possible_roles.approver.push.apply(uniqueArray);
     }catch{
         callback(`Internal Server Error has occured`,null);
         return;
